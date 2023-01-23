e8690a32f22f8aed9a10b097e70d5e973815cc0a
MUNAH10 <munahassan5075@gmail.com>
2023-01-23 23:43:47 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import GifListContaainer from "./GifListContainer"
+
 import NavBar from "./NavBar";
 
 // the App component should render out the GifListContainer component
@@ -8,6 +10,7 @@ function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GifListContaainer />
     </div>
   );
 }
